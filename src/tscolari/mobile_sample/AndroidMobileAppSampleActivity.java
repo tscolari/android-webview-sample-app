7e8eaffe2dea337ab8e875fa327528da51e070e3
Tiago Scolari <tscolari@gmail.com>
2011-09-19 10:43:48 -0300

    webview mobile app sample

---
+++ src/tscolari/mobile_sample/AndroidMobileAppSampleActivity.java
@@ -0,0 +1,35 @@
+package tscolari.mobile_sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AndroidMobileAppSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
+        
+        WebSettings webSettings = mainWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        
+        mainWebView.setWebViewClient(new MyCustomWebViewClient());
+        mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        
+        mainWebView.loadUrl("http://mobile-sample-app.heroku.com");
+    }
+    
+    private class MyCustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
